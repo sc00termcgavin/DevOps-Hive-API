5d54c1cd2e5b5bb7005f731c8318c17237b8df6a
sc00termcgavin <suspiciously.unknown@pm.me>
2024-12-09 19:06:38 -0700

    feat(temperature): added temperature endpoint making sure data is not older than 1 hour.

--- README.md
+++ README.md
@@ -5,6 +5,29 @@ A RESTful API to track environmental sensor data for beekeepers using openSenseM
 
 The goal of this project is to build a scalable RESTful API around [openSenseMap](https://opensensemap.org/) but customized to help beekeeper with their chores. The API output should be in JSON. You will start with a basic implementation, then extend the whole system to handles thousands of requests per second.
 
+### IDs
+senseBox IDs:
+1. edmonds-WA
+   1. ID: 6351780cc18329001ba8d4a3
+      1. 11.63
+      2. 024-12-10T00:40:57.172Z
+   2. https://api.opensensemap.org/boxes/6351780cc18329001ba8d4a3
+   3. https://opensensemap.org/explore/6351780cc18329001ba8d4a3
+2. 97tug
+      1. 7.955882
+      2. 2024-12-10T00:40:19.586Z
+   1. ID: 63ac947f1aaa3a001b8a34bd
+   2. https://api.opensensemap.org/boxes/63ac947f1aaa3a001b8a34bd
+   3. https://opensensemap.org/explore/63ac947f1aaa3a001b8a34bd
+3. rakwanna
+      1. 4.7
+      2. 024-12-10T00:40:15.616Z
+   1. ID: 62abdbfbb91502001b7c05a8
+   2. https://api.opensensemap.org/boxes/62abdbfbb91502001b7c05a8
+   3. https://opensensemap.org/explore/62abdbfbb91502001b7c05a8
+
+
+
 
 ### Tasklist
 
@@ -17,6 +40,21 @@ The goal of this project is to build a scalable RESTful API around [openSenseMap
 - [x] Test
   - [x] Run the app container and make sure it returns the correct value. 
 
+**Phase 3**
+- [ ] Code:
+  - [x] Version Endpoint: `/version`
+    - [x] Returns the version of the deployed app.
+  - [x] Temperature Endpoint: `/temperature`
+    - [x] Return current average temperature based on all senseBox data.
+    - [x] Ensure that the data is no older 1 hour.
+- [ ] Continuous Integration
+  - [ ] Create a GitHub Actions workflow for CI.
+  - [ ] Add step to lint code and Dockerfile.
+  - [ ] Add step to build the Docker image.
+  - [ ] Add step to unit tests.
+  - [ ] Setup OpenSSF Scorecard GitHub Action and fix any issues reported by it.
+- [ ] Test
+  - [ ] In the CI pipeline, call the /version endpoint and ensure that it returns the correct value.
 
 ### Tools
 
--- main.py
+++ main.py
@@ -1,10 +1,88 @@
-import sys
-# Print app version
+import requests
+from fastapi import FastAPI
+from datetime import datetime, timezone
 
-def print_version():
-    version = 'v0.0.1'
-    print(f"App Version: {version}")
-    sys.exit(0)
+app = FastAPI()
 
-if __name__ == "__main__":
-    print_version()  # Calling the function to print the version and exit
+
+# Version Endpoint
+@app.get("/version")
+async def get_version():
+    version = version = "v0.1.0"
+    return {"version": version}
+
+
+# format: https://api.opensensemap.org/boxes/:senseBoxId?format=:format
+
+# Temperature Endpoint
+@app.get("/temperature")
+def get_temp():
+    url1= 'https://api.opensensemap.org/boxes/6351780cc18329001ba8d4a3?format=json'
+    url2= 'https://api.opensensemap.org/boxes/63ac947f1aaa3a001b8a34bd?format=json'
+    url3= 'https://api.opensensemap.org/boxes/62abdbfbb91502001b7c05a8?format=json'
+    
+    # get request to fetch data from the API
+    response1 = requests.get(url1)
+    response2 = requests.get(url2)
+    response3 = requests.get(url3)
+
+    now = datetime.now(timezone.utc)
+
+    #print(response.status_code)
+    #print(response.json())
+
+    # 1
+    # access sensors array, 2nd index, 3rd object 
+    temp1 = float(response1.json()['sensors'][2]['lastMeasurement']['value'])
+    time1 = response1.json()['sensors'][2]['lastMeasurement']['createdAt']
+
+    # Parse the createdAt timestamp from the API response
+    timestamp1 = datetime.fromisoformat(time1.replace("Z", "+00:00"))
+    # Calculate the difference between the current time and the createdAt time
+    time_difference1 = now - timestamp1
+    # Check if the data is older than 1 hour (3600 seconds)
+    if time_difference1.total_seconds() > 3600:  
+        print("The data is older than 1 hour.")
+    else:
+        print(time1,"Data is within the last hour.")
+
+    print(temp1)
+
+    # 2
+
+    temp2 = float(response2.json()['sensors'][0]['lastMeasurement']['value'])
+    time2 = response2.json()['sensors'][0]['lastMeasurement']['createdAt']
+
+    timestamp2 = datetime.fromisoformat(time1.replace("Z", "+00:00"))
+    time_difference2 = now - timestamp2
+    if time_difference2.total_seconds() > 3600:  
+        print("The data is older than 1 hour.")
+    else:
+        print(time2,"Data is within the last hour.")
+
+    print(temp2)
+
+    # 3
+    temp3 = float(response3.json()['sensors'][2]['lastMeasurement']['value'])
+    time3 = response3.json()['sensors'][2]['lastMeasurement']['createdAt']
+
+    timestamp3 = datetime.fromisoformat(time1.replace("Z", "+00:00"))
+    time_difference3 = now - timestamp3
+    if time_difference3.total_seconds() > 3600:  
+        print("The data is older than 1 hour.")
+    else:
+        print(time3,"Data is within the last hour.")
+
+    print(temp3)
+
+
+    print("------")
+    temp_total = round((float(temp1) + float(temp2) + float(temp3) ) / 3, 2)
+
+    print(temp_total)
+    return(temp_total)
+
+
+# Run the function
+if __name__ == '__main__':
+    get_temp()
